f136469fb339709da0c4b80278db9a31ec4b482d
Changjun Kim <beaican530@gmail.com>
2021-05-23 12:42:53 +0900

    Connect docker mariadb on EC2 with django restapi server

--- airsm_restapi/settings.py
+++ airsm_restapi/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'airsm_restapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'airsm',
+        'USER': 'root',
+        'PASSWORD': 'mariadb',
+        'HOST': '13.208.94.244',
+        'PORT': '3306',
     }
 }
 
