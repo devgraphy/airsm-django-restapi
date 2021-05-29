02939cfc344571acbcb5a3cf027e11b71daeb64d
Changjun Kim <beaican530@gmail.com>
2021-05-29 14:27:23 +0900

    add urls:signin

--- membership/urls.py
+++ membership/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import helloAPI, userList, getUser, getRank
+from .views import helloAPI, userList, getUser, getRank,signIn
 
 # 순서 규칙
 urlpatterns = [
     path('rank/',getRank),
     path('hello/',helloAPI),
     path('users/',userList),
+    path('signin/',signIn),
     path('<str:id>/',getUser),
 ]
