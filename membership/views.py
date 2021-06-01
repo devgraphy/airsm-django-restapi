722db45da2267fff595b18b2a6b6e891b32e18bc
Changjun Kim <beaican530@gmail.com>
2021-06-01 16:41:41 +0900

    final

--- membership/views.py
+++ membership/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from .models import Member, Discharge
 from .serializers import MemberSerializer, DischargeSerializer
+from django.core.exceptions import ObjectDoesNotExist
+import json
 # Create your views here.
 
 # Create your views here.
@@ -30,11 +32,10 @@ def getUser(request, id):
 def getRank(request):
     rank = Member.objects.order_by('-points')
     serializer = MemberSerializer(rank, many=True)
+    print(rank)
     return Response(serializer.data)
 
-<<<<<<< HEAD
-
-=======
+# 회원가입
 @api_view(['POST'])
 def memberCreate(request):
     print(request.data)
@@ -43,6 +44,43 @@ def memberCreate(request):
         print("Hi")
         serializer.save()
         return Response(serializer.data,status=200)
+    print(serializer.errors)
     return Response("member create error",status=400)
-   
->>>>>>> 555ba333813b178548eb462e9bf32e77677eb225
+
+# 로그인
+@api_view(['POST'])
+def login(request):
+    print(request.data)
+    # value 추출
+    # phone이 DB에 존재하는지 여부
+    p = request.data.get('phone')
+    print(p)
+    pw = request.data.get('password')
+    print(pw)
+    if(Member.objects.get(phone=p)):
+        m = Member.objects.get(phone=p)
+        if(pw == m.password):
+            print(m.password)
+            serializer = MemberSerializer(m)
+            return Response(serializer.data,200)
+    else:
+        return Response("no",status=400)
+
+@api_view(['POST'])
+def point(request):
+    num = 50
+    p = request.data.get('phone')
+    print(request)
+    try:
+        obj = Member.objects.get(phone=p)
+        obj.points+=num
+        obj.save()
+    except ObjectDoesNotExist:
+        serializer = MemberSerializer(data = request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+        obj = Member.objects.get(phone=p)
+        obj.points+=num
+        obj.save()
+    return Response(num, status=200)
+        
