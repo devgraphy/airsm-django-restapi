a11e6cb855fa710e795faa90d8a619a0b102cd7b
Changjun Kim <beaican530@gmail.com>
2021-06-07 01:39:01 +0900

    Update model:member-email-null allowed

--- membership/models.py
+++ membership/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Member(models.Model):
     phone = models.CharField(max_length=11, primary_key=True)
     name = models.CharField(max_length=30, null=True)
-    email = models.EmailField()
+    email = models.EmailField(null=True)
     points = models.IntegerField(default=0)
     password = models.CharField(max_length=30,null=True)
     def __str__(self):
